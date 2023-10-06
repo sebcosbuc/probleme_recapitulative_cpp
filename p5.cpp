d8de16c8ce724ba2364ca2752a2ac67e9a39255a
sebcosbuc <145281065+sebcosbuc@users.noreply.github.com>
2023-10-06 20:47:23 +0300

    Create p5.cpp

---
+++ p5.cpp
@@ -0,0 +1,14 @@
+/*Sa se afiseze puterea x a unui numar intreg si radicalul acestuia*/
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n,x;
+    cin>>n;
+    cin>>x;
+    cout<<pow(n,x)<<endl;
+    cout<<sqrt(n);
+    return 0;
+}
